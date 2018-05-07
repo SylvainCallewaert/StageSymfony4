6307e4c96eea70352949e4c1daf74475f343c405
Muzubi <sylvaincallewaert@rocketmail.com>
2018-05-07 16:13:50 +0200

    Changement du mode de connexion en fos-userbundle au lieu de in_memorie dans la securité

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Entity;
+
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @ORM\Table(name="fos_user")
  */
 class User extends BaseUser
 {
@@ -23,25 +24,35 @@ class User extends BaseUser
      */
     protected $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $name;
+//    /**
+//     * @ORM\Column(type="string", length=255)
+//     *
+//     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+//     * @Assert\Length(
+//     *     min=3,
+//     *     max=255,
+//     *     minMessage="The name is too short.",
+//     *     maxMessage="The name is too long.",
+//     *     groups={"Registration", "Profile"}
+//     * )
+//     */
+//    protected $name;
 
     public function getId()
     {
         return $this->id;
     }
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
+//    public function getName(): ?string
+//    {
+//        return $this->name;
+//    }
+//
+//    public function setName(string $name): self
+//    {
+//        $this->name = $name;
+//
+//        return $this;
+//    }
 
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
 }
